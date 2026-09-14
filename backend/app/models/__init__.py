f32daa3f4205ad1c01ed76917e67a93c619914d1
BaoBD <em123chim@gmail.com>
2026-09-14 11:28:54 +0700

    xoa backend cua khiem

--- backend/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.models.user import Role, User
-
-__all__ = ["Role", "User"]
--- backend/app/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.schemas.auth import Token, UserLogin, UserRegister, UserResponse
-
-__all__ = ["Token", "UserLogin", "UserRegister", "UserResponse"]
